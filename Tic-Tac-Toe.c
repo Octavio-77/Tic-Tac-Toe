a605af859cd46920d962b1c7a7068bf0a2391c04
Octavio <98127915+Octavio-77@users.noreply.github.com>
2022-07-06 16:00:56 -0300

    new 2 game modes version

--- Tic-Tac-Toe.c
+++ Tic-Tac-Toe.c
@@ -1,92 +1,161 @@
-//Tik-Tak-Toe.c: 1vs1 mode
-
+//Tik-Tak-Toe: 2 game modes.
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void showTable(char table[]);
 int gameStatus(char table[], int turns);
+void machine_turn(char table[]);
+void mode1(int turns, char table[]); // 1vs1
+void mode2(int turns, char table[]); // playerVSmachine
+int playerTurn(int turns, char table[]);
 
 int main(){
-	char table[10] = {'1','2','3','4','5','6','7','8','9','\0'};
-	int currentTurn;
-	int turns;
-	int status = 0; // 0 = draw | 1 = p1 win | 2 = p2 win
+	int gamemode;
+	do{
+		char table[10] = {'1','2','3','4','5','6','7','8','9','\0'};
+		int currentTurn;
+		int turns;
+		puts("New Game");
+		printf("Select the game mode:\n"
+			"[1]: 1vs1.\n"
+			"[2]: Player vs Machine.\n"
+			"[3]: Exit.\n");
+		scanf("%d", &gamemode);
+		if(gamemode == 1){
+			puts("1vs1 Mode:");
+			mode1(turns, table);
+		}
+		else if(gamemode == 2){
+			puts("Player vs Machine Mode:");
+			mode2(turns, table);
+		}
+		else if(gamemode == 3){
+			break;
+		}
+		else{
+			puts("Option doesn't exist");
+		}
+	}while(gamemode);
+}
 
-	for(turns = 1; turns <= 9; ++turns){
-		printf("Player n°%s turn\n", (turn % 2 != 0) ? "1":"2");
+int playerTurn(int turns, char table[]){
+		int currentTurn;
 		showTable(table);
 		printf("Choose a number: ");
 		scanf("%d", &currentTurn);
 
 		if(currentTurn <= 9 && currentTurn >= 1){
 			if(table[currentTurn-1] != 'X' && table[currentTurn-1] != 'O'){
-				if(turns == 1 || turns == 3 || turns == 5 || turns == 7 || turns == 9){
+				if(turns % 2 != 0){
 					table[currentTurn-1] = 'X';
 				}
 				else{
 					table[currentTurn-1] = 'O';
 				}
+				return 0;
 
-				status = gameStatus(table, turns);
-			
-				if(status == 1) break;
-				else if(status == 2) break;
 			}
 			else{
 				puts("Invalid number, try again.");
-				--turns;
+				return 1;
 			}
-			
-
 		}
 		else{
 			puts("Invalid number, try again.");
+			return 1;
+
+		}
+}
+
+
+// 1 vs 1:
+void mode1(int turns, char table[]){
+	for(turns=1; turns<=9; ++turns){
+		printf("turns = %d\n", turns);
+		printf("\n\nPlayer n°%s turn\n", (turns % 2 != 0) ? "1":"2");
+		if(playerTurn(turns, table)){
 			--turns;
 		}
+
+		// 0 = draw	; 1 = p1 win ; 2 = p2 win.		
+		if((gameStatus(table, turns)) != 0) {
+			showTable(table);
+			printf("Game finalized: %s wins\n", (gameStatus(table, turns) % 2 != 0) ? "player1":"player2");
+			break;
+		}
+
+		if(turns == 9){
+			showTable(table);
+			puts("Game finalized in a draw");
+		}
 	}
-	showTable(table);
-	if(status == 1 || status == 2){
-		printf("Game finalized, player n°%d wins\n", status);
+}
+
+// playerVSmachine:
+void mode2(int turns, char table[]){
+	for(turns=1; turns<=9; ++turns){
+		printf("\n\nPlayer n°%s turn\n", (turns % 2 != 0) ? "1":"2");
+		if(turns % 2 != 0){
+			if(playerTurn(turns, table)){
+				--turns;
+			}
+		}
+		else{
+			machine_turn(table);
+		}
+
+		// 0 = draw	; 1 = p1 win ; 2 = p2 win.		
+		if((gameStatus(table, turns)) != 0) {
+			showTable(table);
+			printf("Game finalized: %s wins\n", (gameStatus(table, turns) % 2 != 0) ? "player1":"Machine");
+			break;
+		}
+
+		if(turns == 9){
+			showTable(table);
+			puts("Game finalized in a draw");
+		}
 	}
-	else{
-		puts("Game finalized in a draw");
-	}
+}
+
+
+void machine_turn(char table[]){
+	size_t i;
+	do{
+		i = 1 + (rand() % 8);
+	}while(table[i-1] == 'X' || table[i-1] == 'O');
+	
+	table[i-1] = 'O';
+	printf("Machine choose: %lu\n",i);
 }
 
 void showTable(char table[]){
+	for(size_t i =0; i<=8; i += 3){
+		puts("-------------");
+		printf("| %c | %c | %c |\n", table[i], table[i+1], table[i+2]);		
+	}
 	puts("-------------");
-	printf("| %c | %c | %c |\n", table[0], table[1], table[2]);
-	puts("-------------");
-	printf("| %c | %c | %c |\n", table[3], table[4], table[5]);
-	puts("-------------");
-	printf("| %c | %c | %c |\n", table[6], table[7], table[8]);
-	puts("-------------");
-
 }
 
 int gameStatus(char table[], int turns){
-	int player;
-	if(turns % 2 != 0){
-		player = 1;
-	}
-	else{
-		player = 2;
-	}
 
 	if((table[0] == table[1]) && (table[0] == table[2]))
-		return player;
+		return turns;
 	else if((table[0] == table[4]) && (table[0] == table[8]))
-		return player;
+		return turns;
 	else if((table[0]== table[3]) && (table[0]== table[6]))
-		return player;
+		return turns;
 	else if((table[3]== table[4]) && (table[3]== table[5]))
-		return player;
+		return turns;
 	else if((table[1]== table[4]) && (table[1]== table[7]))
-		return player;
+		return turns;
 	else if((table[6]== table[7]) && (table[6]== table[8]))
-		return player;
+		return turns;
 	else if((table[6]== table[4]) && (table[6]== table[2]))
-		return player;
+		return turns;
 	else if((table[2]== table[5]) && (table[2]== table[8]))
-		return player;
+		return turns;
 	else
 		return 0;
 }
